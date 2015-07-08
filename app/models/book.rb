5302c784f6485292d2f2098600031a174d98ce30
Patrick Nielsen <ptnielsen55@gmail.com>
2015-07-08 14:24:06 -0400

    Add Relationships to Models

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ActiveRecord::Base
+  has_many :users, through: :checkout_histories
+
+  validates_presence_of :title, :author, :campus
 end
--- app/models/checkout_history.rb
+++ app/models/checkout_history.rb
@@ -1,2 +1,6 @@
 class CheckoutHistory < ActiveRecord::Base
+  belongs_to :user
+  belongs_to :book
+
+  validates_presence_of :user_id, :book_id, :due_date 
 end
